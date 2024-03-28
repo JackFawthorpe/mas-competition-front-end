7fac34b83e29d65c46994345a00fd5d004b948c2
Jack Fawthorpe <fawthorp878@gmail.com>
2024-03-29 12:04:10 +1300

    Infrastructure: Added context based routing

--- src/apis/configs/axiosConfigs.js
+++ src/apis/configs/axiosConfigs.js
@@ -1,9 +1,17 @@
 
-import axios from "axios"
+import axios from "axios";
 
+let baseURL = "http://localhost:4300/api/v1";
+
+if (process.env.NODE_ENV === "production") {
+    baseURL = "https://localhost:4000/api/v1";
+  } else if (process.env.NODE_ENV === "staging") {
+    baseURL = "https://localhost:4500/api/v1";
+  }
+  
 export const api = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
-})
+baseURL: baseURL
+});
 
 // defining a custom error handler for all APIs
 const errorHandler = (error) => {
