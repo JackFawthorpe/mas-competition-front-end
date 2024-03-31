8e7775db945d38adc88e2bd1c1d14d1d0f8fbc48
Jack Fawthorpe <fawthorp878@gmail.com>
2024-03-31 17:54:37 +1300

    Added way to differentiate Prod

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,7 +11,7 @@ import { AuthContext } from '../context/AuthContext'
 
 const useStyles = makeStyles(() => ({
   sidePanel: {
-    background: 'radial-gradient(circle at 10% 20%, rgb(252, 251, 121) 0%, #ffb74d 90%)',
+    background: process.env.PUBLIC_URL !== '/prod' ? 'radial-gradient(circle at 10% 20%, rgb(46, 204, 113) 0%, rgb(52, 152, 219) 90%)' : 'radial-gradient(circle at 10% 20%, rgb(252, 251, 121) 0%, #ffb74d 90%)',
     height: '100%',
     width: '65%',
     boxShadow: '3px 0px 5px rgba(0, 0, 0, 0.25)'
